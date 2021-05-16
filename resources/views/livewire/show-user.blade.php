aa34a43ed8ed453e5a34c41780caf63b4234a1a2
JuanJosexD <juanjosexdd7@gmail.com>
2021-05-16 19:45:23 -0400

    subida a heroku

--- resources/views/livewire/show-user.blade.php
+++ resources/views/livewire/show-user.blade.php
@@ -92,23 +92,23 @@
                                 @if ($user->estatus == 1)
                                     <form class="formulario-estatus" action="{{route('admin.users.show', $user)}}" method="get">
                                         @csrf
-                                        <button type="submit" class="btn btn-outline-success btn-sm elevation-4"><i class="fas fa-user-check"></i></button>
+                                        <button type="submit" class="btn text-yellow"><i class="fas fa-user-check"></i></button>
                                     </form>
                                 @else
                                 <form class="formulario-estatus" action="{{route('admin.users.show', $user)}}" method="get">
                                     @csrf
-                                    <button type="submit" class="btn btn-outline-danger btn-sm elevation-4"><i class="fas fa-user-times"></i></button>
+                                    <button type="submit" class="btn text-blue"><i class="fas fa-user-times"></i></button>
                                 </form>
                                 @endif
                             </td>
                             <td width="4px">
-                                <a class="btn btn-outline-info btn-sm mr-1 elevation-4" href=" {{route('admin.users.edit',$user)}} "><i class="fas fa-edit"></i></a>
+                                <a class="btn text-blue" href=" {{route('admin.users.edit',$user)}} "><i class="fas fa-edit"></i></a>
                             </td>
                             <td width="4px">
                                 <form class="formulario-eliminar" action="{{route('admin.users.destroy', $user)}}" method="POST">
                                     @csrf
                                     @method('delete')
-                                    <button type="submit" class="btn btn-outline-danger btn-sm elevation-4"><i class="fas fa-trash"></i></button>
+                                    <button type="submit" class="btn text-danger"><i class="fas fa-trash"></i></button>
                                 </form>
                             </td>
                         </tr>
